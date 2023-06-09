71af869b5666d0504630399bfee697e5d77f77ec
DevAntonioRogers <ar919061493@gmail.com>
2023-06-09 00:00:42 -0400

    Projects

---
+++ app/components/Projects.jsx
@@ -0,0 +1,71 @@
+import Image from "next/image";
+import "./Projects.css";
+import project1 from "../../public/project-1.png";
+import { BsFillArrowUpRightCircleFill } from "react-icons/bs";
+
+const Projects = () => {
+  return (
+    <section className="projects">
+      <svg width="0" height="0">
+        <linearGradient id="gradient" x1="100%" y1="100%" x2="0%" y2="0%">
+          <stop stop-color="#3f5efb" offset="0%" />
+          <stop stop-color="#fc466b" offset="100%" />
+        </linearGradient>
+      </svg>
+
+      <h1>Projects</h1>
+      <div className="projects-container">
+        <div className="projects-image">
+          <Image src={project1} width={0} height={0} />
+          <a href="https://tailwind-responsive-navbar-mu.vercel.app/" target="_blank">
+            <div className="icon">
+              <BsFillArrowUpRightCircleFill size={100} style={{ fill: "url(#gradient)" }} />
+            </div>
+          </a>
+        </div>
+        <div className="projects-image">
+          <Image src={project1} width={0} height={0} />
+          <a href="https://tailwind-responsive-navbar-mu.vercel.app/" target="_blank">
+            <div className="icon">
+              <BsFillArrowUpRightCircleFill size={100} style={{ fill: "url(#gradient)" }} />
+            </div>
+          </a>
+        </div>
+        <div className="projects-image">
+          <Image src={project1} width={0} height={0} />
+          <a href="https://tailwind-responsive-navbar-mu.vercel.app/" target="_blank">
+            <div className="icon">
+              <BsFillArrowUpRightCircleFill size={100} style={{ fill: "url(#gradient)" }} />
+            </div>
+          </a>
+        </div>
+        <div className="projects-image">
+          <Image src={project1} width={0} height={0} />
+          <a href="https://tailwind-responsive-navbar-mu.vercel.app/" target="_blank">
+            <div className="icon">
+              <BsFillArrowUpRightCircleFill size={100} style={{ fill: "url(#gradient)" }} />
+            </div>
+          </a>
+        </div>
+        <div className="projects-image">
+          <Image src={project1} width={0} height={0} />
+          <a href="https://tailwind-responsive-navbar-mu.vercel.app/" target="_blank">
+            <div className="icon">
+              <BsFillArrowUpRightCircleFill size={100} style={{ fill: "url(#gradient)" }} />
+            </div>
+          </a>
+        </div>
+        <div className="projects-image">
+          <Image src={project1} width={0} height={0} />
+          <a href="https://tailwind-responsive-navbar-mu.vercel.app/" target="_blank">
+            <div className="icon">
+              <BsFillArrowUpRightCircleFill size={100} style={{ fill: "url(#gradient)" }} />
+            </div>
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Projects;
--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,13 @@
 import Divider from "./components/Divider";
 import Hero from "./components/Hero";
+import Projects from "./components/Projects";
 
 const Home = () => {
   return (
     <>
       <Hero />
       <Divider />
+      <Projects />
     </>
   );
 };
