db2afc5057bccd8ec5abf53cd0969c35053ea5bf
DevAntonioRogers <ar919061493@gmail.com>
2023-06-08 20:50:13 -0400

    Nav

---
+++ app/components/Nav.jsx
@@ -0,0 +1,40 @@
+"use client";
+
+import "./Nav.css";
+import { useState } from "react";
+
+const Nav = () => {
+  const [openMenu, setOpenMenu] = useState(false);
+
+  const menuHandler = () => {
+    setOpenMenu(!openMenu);
+  };
+  return (
+    <nav>
+      <div className="nav-container">
+        <h1>
+          Antonio<span>.com</span>
+        </h1>
+
+        <ul className={openMenu ? "nav-menu active" : "nav-menu"}>
+          <li className="active">Home</li>
+          <li>Portfolio</li>
+          <li>Contact</li>
+          <li>About</li>
+        </ul>
+
+        <div className="nav-right">
+          <button>Hire Me</button>
+
+          <div onClick={menuHandler} className={openMenu ? "hamburger active" : "hamburger"}>
+            <span className="bar"></span>
+            <span className="bar"></span>
+            <span className="bar"></span>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
