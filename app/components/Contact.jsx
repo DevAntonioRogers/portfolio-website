7b744b05900b67cf377024ff2b402051d6c5293c
DevAntonioRogers <ar919061493@gmail.com>
2023-06-10 01:47:35 -0400

    Contact Styling

--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -21,16 +21,29 @@ export const Contact = () => {
   };
 
   return (
-    <form ref={form} onSubmit={sendEmail}>
-      <h1>Send me a message</h1>
-      <label htmlFor="name">Name:</label>
-      <input type="text" id="name" name="name" required />
-      <label htmlFor="email">Email:</label>
-      <input type="email" id="email" name="email" required />
-      <label htmlFor="reason">Reason:</label>
-      <textarea id="reason" name="reason" required></textarea>
-      <button type="submit">Send</button>
-    </form>
+    <div className="contact">
+      <div className="contact-container">
+        <div className="contact-text">
+          <h1>
+            Let's Chat. <br /> Tell me about your project.
+          </h1>
+          <span>Let's create something together ✨</span>
+        </div>
+        <form ref={form} onSubmit={sendEmail}>
+          <div className="form-heading">
+            <h1>Send me a message</h1>
+            <span>🚀</span>
+          </div>
+          <label htmlFor="name">Name:</label>
+          <input type="text" id="name" name="name" required />
+          <label htmlFor="email">Email:</label>
+          <input type="email" id="email" name="email" required />
+          <label htmlFor="reason">Reason:</label>
+          <textarea id="reason" name="reason" required></textarea>
+          <button type="submit">Send</button>
+        </form>
+      </div>
+    </div>
   );
 };
 
