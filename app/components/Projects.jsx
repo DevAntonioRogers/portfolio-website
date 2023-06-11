b010f304ffb81df47cce1dadfc72c543cc300d35
DevAntonioRogers <ar919061493@gmail.com>
2023-06-11 00:11:40 -0400

    Few Bug Fixes

--- app/components/Projects.jsx
+++ app/components/Projects.jsx
@@ -8,8 +8,8 @@ const Projects = () => {
     <section id="projects" className="projects">
       <svg width="0" height="0">
         <linearGradient id="gradient" x1="100%" y1="100%" x2="0%" y2="0%">
-          <stop stop-color="#3f5efb" offset="0%" />
-          <stop stop-color="#fc466b" offset="100%" />
+          <stop stopColor="#3f5efb" offset="0%" />
+          <stop stopColor="#fc466b" offset="100%" />
         </linearGradient>
       </svg>
 
